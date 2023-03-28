f3deada12a7d96d96b13969c833e3908ae3d2dc4
Maxim Khotsevich <mkhotsevich@gmail.com>
2023-03-28 21:03:38 +0300

    prepare app

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-import nodeLogo from './assets/node.svg'
-import { useState } from 'react'
-import Update from '@/components/update'
-import './App.scss'
+import "./App.scss";
+import { FC } from "react";
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
+const App: FC = () => {
+  return <>"Hello World"</>;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <div className='App'>
-      <div>
-        <a href='https://github.com/electron-vite/electron-vite-react' target='_blank'>
-          <img src='./electron-vite.svg' className='logo' alt='Electron + Vite logo' />
-        </a>
-      </div>
-      <h1>Electron + Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Electron + Vite logo to learn more
-      </p>
-      <div className='flex-center'>
-        Place static files into the<code>/public</code> folder <img style={{ width: '5em' }} src={nodeLogo} alt='Node logo' />
-      </div>
-
-      <Update />
-    </div>
-  )
-}
-
-export default App
+export default App;
